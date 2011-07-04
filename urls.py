76c28d2f778c877a97b72ab76537db2bc424492f
Christian Spanring <cspanring@gmail.com>
2011-07-03 22:38:35 -0400

    serve media files (uploads) in dev environment

--- urls.py
+++ urls.py
@@ -50,3 +50,11 @@ if settings.SERVE_MEDIA:
     )
 
 
+# serve media (uploads) on dev server
+if settings.DEBUG:
+	from django.views.static import serve
+	_media_url = settings.MEDIA_URL
+	if _media_url.startswith('/'):
+		_media_url = _media_url[1:]
+		urlpatterns += patterns('', (r'^%s(?P<path>.*)$' % _media_url, serve, {'document_root': settings.MEDIA_ROOT}))
+	del(_media_url, serve)
