de242392892321c8299cd022574d4ac86939d9fa
Christian Spanring <cspanring@gmail.com>
2011-07-07 22:48:49 -0400

    Added some custom fields to user profile
    my station, flickr, twitter, youtube and facebook.

--- apps/profiles/models.py
+++ apps/profiles/models.py
@@ -2,10 +2,15 @@ from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
 from idios.models import ProfileBase
+from participation.models import Station
 
 
 class Profile(ProfileBase):
-    name = models.CharField(_("name"), max_length=50, null=True, blank=True)
-    about = models.TextField(_("about"), null=True, blank=True)
-    location = models.CharField(_("location"), max_length=40, null=True, blank=True)
-    website = models.URLField(_("website"), null=True, blank=True, verify_exists=False)
+	name = models.CharField(_("name"), max_length=50, null=True, blank=True)
+	about = models.TextField(_("about"), null=True, blank=True)
+	website = models.URLField(_("website"), null=True, blank=True, verify_exists=False)
+	mystation = models.ForeignKey(Station, null=True, blank=True)
+	flickr_id = models.CharField("My Flickr username", max_length=50, null=True, blank=True)
+	youtube_id = models.CharField("My YouTube username", max_length=50, null=True, blank=True)
+	twitter_id = models.CharField("My Twitter username", max_length=50, null=True, blank=True)
+	facebook_id = models.CharField("My Facebook username", max_length=50, null=True, blank=True)
