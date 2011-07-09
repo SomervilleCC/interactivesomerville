56431af04bc7cdda6ae59a94c4a8eea90ac5c122
Christian Spanring <cspanring@gmail.com>
2011-07-09 10:31:44 -0400

    added pagination to activity stream on profile page

--- apps/participation/templatetags/participation_tags.py
+++ apps/participation/templatetags/participation_tags.py
@@ -1,10 +1,24 @@
 from django import template
 from django.template.defaultfilters import stringfilter
 
+from participation.models import Shareditem
+
 register = template.Library()
+
 
 @register.filter
 @stringfilter
 def fixbackslash(value):
 	"""Replace backslashes '\'  in encoded polylines for Google Maps overlay."""
 	return value.replace('\\','\\\\')
+
+
+def get_activity(activity):
+	if activity.itemtype == "e" and activity.url:
+		resource = activity.get_oembed()
+
+	return {
+		'activity': activity,
+		'resource': resource,
+	}
+register.inclusion_tag("participation/_activity.html")(get_activity)
